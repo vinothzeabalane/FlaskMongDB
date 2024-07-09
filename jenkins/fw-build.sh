a1d69b5601472b5bbf977fb291d8266a63d7bc2f
zeabalane <omprakash.aroumougam@gmail.com>
2024-07-08 17:25:58 -0700

    jenkins job

---
+++ jenkins/fw-build.sh
@@ -0,0 +1,21 @@
+echo "********************** PULL the lastest trenton repository code ******************"
+cd; cd /home/jenkins/repos/trenton;
+sudo git reset --hard; git pull
+git log -1
+
+
+echo "********* COMPILE THE BINARY *******"
+cd; cd /home/jenkins/repos/trenton;
+sudo echo -e '\n source /home/jenkins/repos/trenton/hud/build-hudhwfwprep.sh -v kv7 -r c0' > /home/jenkins/repos/ps_bash.sh
+sudo cat /home/ozeabalx/env.sh /home/jenkins/repos/ps_bash.sh > /home/jenkins/repos/final_bash.sh
+sudo chmod u+x /home/jenkins/repos/final_bash.sh
+sudo cp -r /home/jenkins/repos/final_bash.sh /home/jenkins/repos/trenton/
+cd /home/jenkins/repos/trenton;
+sudo ./final_bash.sh
+cd /home/jenkins/repos/trenton/prod/hud/debug/hwfwprep-kv7/build/eeprom; ls;
+echo "********************** FW Build binary Ended ******************"
+
+
+cd /mnt/udrive/ozeabalx/ps-bootprofile; sudo rm -r *
+sudo cp -r /home/jenkins/repos/trenton/prod/hud/debug/hwfwprep-kv7/build/eeprom /mnt/udrive/ozeabalx/ps-bootprofile
+sudo cp -r /home/jenkins/repos/trenton/prod/hud/debug/hwfwprep-kv7/build/unified_image/unified_image.bin /mnt/udrive/ozeabalx/ps-bootprofile
