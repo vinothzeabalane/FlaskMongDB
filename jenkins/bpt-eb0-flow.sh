378f35e320564f2cf31faf0dd939c91bf594f7e9
zeabalane <omprakash.aroumougam@gmail.com>
2024-07-21 16:02:21 -0700

    bpt flow

--- jenkins/bpt-eb0-flow.sh
+++ jenkins/bpt-eb0-flow.sh
@@ -6,7 +6,7 @@ echo "PYTHONPATH: $PYTHONPATH"
 
 # Prepare filename with current date
 today=$(date +"%Y-%m-%d")
-filename="chewy20-raw-8TB-SPI-$today.csv"
+filename="chewy20-raw-8TB-EB0-$today.csv"
 
 # Print filename for verification
 echo "Filename: $filename"
@@ -21,12 +21,44 @@ cd /home/remlab || exit
 sudo rm -r /home/remlab/cmdline.cfg /home/remlab/"$filename" 2>/dev/null || true
 
 # Create cmdline.cfg file with commands
+echo -e "selectdev 1\ntc-unlock\nfwdownload -f /mnt/udrive/ozeabalx/ps-bootprofile/unified_image.bin\nfwcommit -s 1 -c 1 -b 0\nexit" > /home/remlab/cmdline.cfg
+
+# Setup environment
+cd /home/jenkins/repos/ent_ssd_test/lib/spdk/scripts/ || exit
+sudo ./setup.sh
+
+# Execute spdktest and append output to $filename
+cd /home/jenkins/repos/ent_ssd_test/spdktest/ || exit
+sudo ./spdktest -t -l /home/remlab/cmdline.cfg >> "/home/remlab/output.log"
+
+
+# Wait for 5 seconds
+sleep 5
+
+# Run pita_cycle
+cd /home/remlab/bhb_tools/ || exit
+sudo ./pita_cycle -a
+
+# Wait for 5 seconds
+sleep 5
+
+# List NVMe devices
+sudo nvme list
+
+# Change directory to /home/remlab
+cd /home/remlab || exit
+
+# Remove files with 'sudo rm -r' and ignore errors if files do not exist
+sudo rm -r /home/remlab/cmdline.cfg /home/remlab/"$filename" 2>/dev/null || true
+
+# Create cmdline.cfg file with commands
 echo -e "selectdev 1\ntc-unlock\ntc-boot-profile\nexit" > /home/remlab/cmdline.cfg
+
 
 # Loop to execute commands 5 times
 for (( i = 1; i <= 5; i++ ))
 do
-    echo "Iteration $i"
+    echo "******************* Iteration $i **************************" >> "/home/remlab/$filename"
 
     # Setup environment
     cd /home/jenkins/repos/ent_ssd_test/lib/spdk/scripts/ || exit
@@ -53,13 +85,3 @@ do
     # Wait for 10 seconds before next iteration
     sleep 10
 done
-
-
-today=$(date +"%Y-%m-%d")
-filename="chewy20-8TB-SPI-$today.csv"
-cd; cd /home/remlab;
-sudo cp -r chewy20-8TB-SPI-$today.csv /mnt/udrive/ozeabalx/ps-bootprofile
-
-
-cd; cd /home/remlab;
-sudo rm -r chewy20-8TB*
--- jenkins/excel-file-format.py
+++ jenkins/excel-file-format.py
@@ -10,13 +10,15 @@ print(today)
 
 # Create workbook and worksheet
 workbook = xlsxwriter.Workbook('/home/remlab/chewy20-8TB-SPI-{}.csv'.format(today))
+# workbook = xlsxwriter.Workbook('C:/Users/ozeabala/OneDrive - NANDPS/Desktop/seyon.csv'.format(today))
 worksheet = workbook.add_worksheet()
 
 # Expected strings to check
-exp_str = [' =================== Selected device 1 =====================', 'boot', 'start', 'time']
+exp_str = [' =================== Selected device 1 =====================', 'boot', 'start', 'time', '******************* Iteration']
 
 # Read CSV file into DataFrame
 df = pd.read_csv('/home/remlab/chewy20-raw-8TB-SPI-{}.csv'.format(today), header=None, sep=';')
+# df = pd.read_csv('C:/Users/ozeabala/OneDrive - NANDPS/Desktop/chewy20-raw-8TB-EB0-{}.csv'.format(today), header=None, sep=';')
 val = df.iloc()
 res = 0
 ignore = True
@@ -35,10 +37,12 @@ for i in range(len(df.columns)):
         print(val.obj[i][j])
         if str(val.obj[i][j]) == 'nan' or check_string(val.obj[i][j]):
             continue
-        if 'Selected device 1' in val.obj[i][j]:
+        if '******************* Iteration' in val.obj[i][j]:
             count = 0
-        if 'Selected device 1' in val.obj[i][j] and not ignore:
+        if '******************* Iteration' in val.obj[i][j] and not ignore:
             res += 1
+        if 'Selected device 1' in str(val.obj[i][j]):
+            continue
         worksheet.write(count, res, val.obj[i][j])
         if str(val.obj[i][j]).startswith('time elapsed in ss.ms.us'):
             count += 1
