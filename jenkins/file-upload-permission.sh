c62f5c4af6f2c762e68c41cfd13e92b015a33102
zeabalane <omprakash.aroumougam@gmail.com>
2024-07-22 20:14:56 -0700

    file upload

---
+++ jenkins/file-upload-permission.sh
@@ -0,0 +1,3 @@
+cd; cd /mnt/udrive/ozeabalx;
+sudo chown -R remlab /mnt/udrive/ozeabalx/ps-bootprofile/*.csv
+sudo chmod 777 /mnt/udrive/ozeabalx/ps-bootprofile/*.csv
--- jenkins/mongodb-files-upload.py
+++ jenkins/mongodb-files-upload.py
@@ -1,7 +1,22 @@
+import os
+import re
+
 from pymongo import MongoClient
 from gridfs import GridFS
 from bson import ObjectId
 from datetime import date
+from pathlib import Path
+
+
+# Specify the directory where CSV files are located
+# directory = '/mnt/udrive/ozeabalx/ps-bootprofile'
+directory = 'C:/Users/ozeabala/OneDrive - NANDPS/Desktop/ps-bootprofile'
+csv_files = []
+commid_id = None
+last_updated = []
+pattern = r'\b\d{4}-\d{2}-\d{2}\b'
+spiflow = False
+eb0flow = True
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
@@ -10,29 +25,48 @@ db = client['openstack']  # Replace with your database name
 # Access GridFS
 fs = GridFS(db)
 
-for i in range(1,20):
-    filename='chewy20-8TB-SPI-2024-07-{}.csv'.format(i)
-    # Example: Reading data from a file
-    with open('C:/Users/ozeabala/OneDrive - NANDPS/Desktop/chewy20-8TB-SPI-2024-07-20.csv', 'rb') as f:
+def read_file(file):
+    with open(file,'r') as f1:
+        return  f1.read()
+
+
+# Iterate through all files in the directory
+for file in os.listdir(directory):
+    # Check if the file is a CSV file
+    if file.startswith("commit_id"):
+        commid_id = read_file(os.path.join(directory, file))
+
+    elif file.startswith("last_updated"):
+        last_updated = re.findall(pattern, read_file(os.path.join(directory, file)))
+
+    elif file.endswith(".csv"):
+        # If yes, add it to the list of CSV files
+        csv_files.append(os.path.join(directory, file))
+
+
+for file in csv_files:
+    filename = os.path.basename(file)
+    is_file_exit = db.fs.files.find_one({"filename": filename})
+    if is_file_exit:
+        print('File name already exists')
+        continue
+    with open(file, 'rb') as f:
         # Store data in GridFS, which handles chunking automatically
         file_id = fs.put(f, filename=filename)
 
-    print(f'Uploaded file with _id: {file_id}')
+    print('Uploaded file with _id: {file_id}')
 
-
+    if 'SPI' in str(filename):
+        spiflow = True
+        eb0flow = False
 
     # Find the uploaded file by filename
     file_info = db.fs.files.find_one({"filename": filename})
     today = date.today()
     date_string = today.strftime("%Y-%m-%d")
 
-
-    if i < 10:
-        new_metadata = {"date": '2024-07-{}'.format(i), "commit_id": "XXXXFFFFFFFFF{}".format(i), "is_spiflow": True,
-        "is_eb0flow": False }
-    else:
-        new_metadata = {"date": '2024-07-{}'.format(i), "commit_id": "XXXXFFFFFFFFF{}".format(i), "is_spiflow": False,
-        "is_eb0flow": True }
+    new_metadata = {"date": last_updated[0], "commit_id": commid_id, "is_spiflow": spiflow,
+        "is_eb0flow": eb0flow, 'file_uploaded_date': date_string }
 
     # Update metadata
     if file_info:
@@ -41,5 +75,9 @@ for i in range(1,20):
         db.fs.files.update_one({"_id": ObjectId(file_id)}, {"$set": {"metadata": new_metadata}})
         
         print("Metadata updated successfully.")
+        Path(os.path.join(directory, filename)).unlink()
+        print("File deleted the shared location")
     else:
         print("File not found.")
+
+
