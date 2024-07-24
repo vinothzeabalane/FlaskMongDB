24a7fea98cdc60b600bd79333ec94125a9a2f0e0
zeabalane <omprakash.aroumougam@gmail.com>
2024-07-23 22:29:19 -0700

    server restart script

---
+++ jenkins/server-restart.py
@@ -0,0 +1,34 @@
+import paramiko
+
+_HOST = [{'ip': '10.74.134.101', 'hostname': 'vc-ps-chewyfit-15', 'login': 'remlab', 'password': '1001SMT!'}, 
+         {'ip': '10.74.134.70', 'hostname': 'vc-ps-chewy-20', 'login': 'remlab', 'password': '1001SMT!'}]
+
+def execute_ssh_command(hostname, port, username, password, command):
+    try:
+        # Create an SSH client instance
+        client = paramiko.SSHClient()
+        # Automatically add host keys from the default file
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # Connect to the server
+        client.connect(hostname, port=port, username=username, password=password)
+        # Execute the command
+        stdin, stdout, stderr = client.exec_command(command)
+        # Read the output from the command
+        output = stdout.read().decode('utf-8')
+        # Print the output
+        print(f"Command executed successfully on {hostname}:\n{output}")
+
+    except Exception as e:
+        print(f"Error executing the command on {hostname}: {str(e)}")
+
+    finally:
+        # Disconnect from the server
+        client.close()
+
+# Example usage:
+if __name__ == "__main__":
+    port = 22  # Default SSH port
+    command = 'hostname; sudo reboot'  # Example command to list files
+
+    for host in _HOST:
+        execute_ssh_command(hostname = host['ip'], port = port, username = host['login'], password= host['password'], command= command)
