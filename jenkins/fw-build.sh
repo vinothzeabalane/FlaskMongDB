a571d10751f451c641ce53790ea7b252ed955ad3
vinothzeabalane <omprakash.aroumougam@gmail.com>
2025-03-11 20:33:39 -0700

    updated fw build bash script

--- jenkins/fw-build.sh
+++ jenkins/fw-build.sh
@@ -1,19 +1,46 @@
-echo "********************** PULL the lastest trenton repository code ******************"
-cd; cd /home/jenkins/repos/trenton;
-sudo git reset --hard; git pull
+#!/bin/bash
+
+# Define paths for easy reference
+REPO_PATH="/home/jenkins/repos/trenton"
+PS_BOOT_COMPILE_PATH="/home/remlab/ps-boot-compile"
+FINAL_BASH_PATH="/home/jenkins/repos/final_bash.sh"
+
+echo "********************** PULL the latest Trenton repository code ******************"
+
+# Remove the existing repository and clone the latest version
+cd /home/jenkins/repos || exit 1
+sudo rm -rf trenton
+git clone ssh://git@npsg-bit.elements.local:7999/fsedev/trenton.git
+echo "********* COMPILE THE BINARY *******"
+
+# Copy required files to Trenton repo
+sudo cp -r $PS_BOOT_COMPILE_PATH/run_time/ $REPO_PATH/configurator/
+sudo cp -r $PS_BOOT_COMPILE_PATH/dependency_metadata.json $REPO_PATH/pfw/shared/fw_update_mic/
+sudo cp -r $PS_BOOT_COMPILE_PATH/env.sh $REPO_PATH
+sudo cp -r $PS_BOOT_COMPILE_PATH/MIC_FW.hex $REPO_PATH/pfw/shared/fw_update_mic/
+
+# Pull the latest changes from the Trenton repo
+cd $REPO_PATH || exit 1
+sudo git pull
 git log -1
 
+# Create and update the final bash script
+echo -e '\n source /home/jenkins/repos/trenton/hud/build-hudhwfwprep.sh -v kv7 -r c0' > $REPO_PATH/ps_bash.sh
+echo -e '\n source ./clean-cmakebuild.sh && /home/jenkins/repos/trenton/hud/build-hudhwfwprep.sh -v kv7 -r c0' | sudo tee -a $REPO_PATH/ps_bash.sh > /dev/null
+sudo cat /home/ozeabalx/env.sh $REPO_PATH/ps_bash.sh > $FINAL_BASH_PATH
 
-echo "********* COMPILE THE BINARY *******"
-cd; cd /home/jenkins/repos/trenton;
-#sudo echo -e '\n source /home/jenkins/repos/trenton/hud/build-hudhwfwprep.sh -v kv7 -r c0' > /home/jenkins/repos/ps_bash.sh
-echo -e '\n source  ./clean-cmakebuild.sh  && /home/jenkins/repos/trenton/hud/build-hudhwfwprep.sh -v kv7 -r c0' | sudo tee /home/jenkins/repos/ps_bash.sh > /dev/null
-sudo cat /home/ozeabalx/env.sh /home/jenkins/repos/ps_bash.sh > /home/jenkins/repos/final_bash.sh
-sudo chmod u+x /home/jenkins/repos/final_bash.sh
-sudo cp -r /home/jenkins/repos/final_bash.sh /home/jenkins/repos/trenton/
-cd /home/jenkins/repos/trenton;
+# Make the final bash script executable and copy it to Trenton repo
+sudo chmod u+x $FINAL_BASH_PATH
+sudo cp -r $FINAL_BASH_PATH $REPO_PATH
+
+# Run the final bash script
+cd $REPO_PATH || exit 1
 sudo ./final_bash.sh
-cd /home/jenkins/repos/trenton/prod/hud/debug/hwfwprep-kv7/build/eeprom; ls;
+
+# List the contents of the final build directory
+cd $REPO_PATH/prod/hud/debug/hwfwprep-kv7/build/eeprom || exit 1
+ls
+
 echo "********************** FW Build binary Ended ******************"
 
 
