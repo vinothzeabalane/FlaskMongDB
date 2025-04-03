642ce250497affee35b314e8892a6ee7728811d2
vinothzeabalane <omprakash.aroumougam@gmail.com>
2025-04-02 17:53:30 -0700

    new-db

---
+++ odoo-custom-addons/ps_inventory/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Platform Service Inventory',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'PS Inventory details - Chewy/ Yoda',
+    'author': 'Omprakash Zeabalane',
+    'website': 'http://www.yourcompany.com',
+    'depends': ['base','mail'],  # List of dependencies (e.g., base, sale, stock, etc.)
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ps_inventory_view.xml',
+        'views/ps_inventory_users_view.xml'
+        # 'views/ps_inventory_remove.xml'
+    ],
+    'installable': True,
+    'application': True,  # Set True if it's an app to be listed in the Apps menu
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
