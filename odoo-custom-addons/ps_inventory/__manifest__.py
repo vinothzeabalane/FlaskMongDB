8afff46f4f7eb7032e1c9b0d19b0791d5c5a72da
vinothzeabalane <omprakash.aroumougam@gmail.com>
2025-04-02 23:15:55 -0700

    clean-up code

--- odoo-custom-addons/ps_inventory/__manifest__.py
+++ odoo-custom-addons/ps_inventory/__manifest__.py
@@ -10,9 +10,14 @@
     'data': [
         'security/ir.model.access.csv',
         'views/ps_inventory_view.xml',
-        'views/ps_inventory_users_view.xml'
-        # 'views/ps_inventory_remove.xml'
+        'views/ps_inventory_users_view.xml',
+        'views/ps_login_templates_view.xml'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'ps_inventory/static/src/**/*',
+        ]
+    },
     'installable': True,
     'application': True,  # Set True if it's an app to be listed in the Apps menu
     'auto_install': False,
